e4e58d81226de8a7a7208b23fa963606ac056c61
Zybartas Ringys <developerzr92@gmail.com>
2022-03-14 20:02:12 +0200

    mobile end

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
-'use strict'
+"use strict";
 
-const cardsContainer = document.getElementById('cards-section')
+const cardsContainer = document.getElementById("cards-section");
 
-const url = 'https://restcountries.com/v2/name/Germany'
+const url = "https://restcountries.com/v2/name/Lithuania";
 
-let data = {}
+let data = {};
+
+console.log(data);
 
 //async fetch function
 
 async function fetchCountry() {
-  const response = await fetch(url)
-  const country = await response.json()
-  country.forEach(element => {
-    data[element.name] = element
-  })
+  try {
+    const response = await fetch(url);
+    const country = await response.json();
+
+    country.forEach((element) => {
+      cardsContainer.insertAdjacentHTML("beforeend", createCard(element));
+    });
+
+    country.forEach((element) => {
+      data[element.name] = element;
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-fetchCountry()
+fetchCountry();
 
-console.log(data)
+function createCard(element) {
+  let title = element["name"];
+  let flag = element["flag"];
+  let population = element["population"];
+  let region = element["region"];
+  let capital = element["capital"];
 
-function createCard() {
-  let title = data['name']
-  console.log(title)
   return `
-    <div class="card">
-                <div class="card-image"></div>
+    <div class="card shadow p-3 mb-5 bg-white rounded">
+                <div class="card-image">
+                <img class="flag" src="${flag}" alt="Flag">
+                </div>
                 <div class="card-body">
-                  <h5 class="card-title">Germany</h5>
-                  <p class="card-text"><span>Population:</span> 81.770.900</p>
-                  <p class="card-text"><span>Region:</span> Europe</p>
-                  <p class="card-text"><span>Capital:</span> Berlin</p>
+                  <h5 class="card-title">${title}</h5>
+                  <p class="card-text"><span>Population:</span> ${population}</p>
+                  <p class="card-text"><span>Region:</span> ${region}</p>
+                  <p class="card-text"><span>Capital:</span> ${capital}</p>
                 </div>
               </div>
-    `
+    `;
 }
 
-createCard()
+createCard();

d92fb0ad1f6864da3ed6e033b919f5fbb831a619
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2022-03-15 19:25:59 +0200

    dark side

--- app.js
+++ app.js
@@ -1,43 +1,69 @@
-"use strict";
+'use strict'
 
-const cardsContainer = document.getElementById("cards-section");
+const cardsContainer = document.getElementById('cards-section')
 
-const url = "https://restcountries.com/v2/all";
+// Themes
+const dark = document.getElementById('Dark-mode')
+const body = document.getElementById('body')
+const navCol = document.querySelector('.navigation-col')
+const whereText = document.querySelector('.where')
+const darkText = document.querySelector('.dark')
+const cardBody = document.querySelectorAll('.card-body')
 
-let data = {};
+const url = 'https://restcountries.com/v2/all'
 
-console.log(data);
+let data = {}
+
+// Dark and light theme
+
+dark.addEventListener('click', switchDark)
+
+// Switch to dark theme function
+
+function switchDark() {
+  body.classList.toggle('active')
+  navCol.classList.toggle('active')
+  whereText.classList.toggle('active')
+  darkText.classList.toggle('active')
+
+  cardBody.forEach(element => {
+    console.log(element)
+    element.classList.toggle('active')
+  })
+}
+
+// API for cards functionality
 
 //async fetch function
 
 async function fetchCountry() {
   try {
-    const response = await fetch(url);
-    const country = await response.json();
+    const response = await fetch(url)
+    const country = await response.json()
 
-    country.forEach((element) => {
-      cardsContainer.insertAdjacentHTML("beforeend", createCard(element));
-    });
+    country.forEach(element => {
+      cardsContainer.insertAdjacentHTML('beforeend', createCard(element))
+    })
 
-    country.forEach((element) => {
-      data[element.name] = element;
-    });
+    country.forEach(element => {
+      data[element.name] = element
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-fetchCountry();
+fetchCountry()
 
 function createCard(element) {
-  let title = element["name"];
-  let flag = element["flag"];
-  let population = element["population"];
-  let region = element["region"];
-  let capital = element["capital"];
+  let title = element['name']
+  let flag = element['flag']
+  let population = element['population']
+  let region = element['region']
+  let capital = element['capital']
 
   return `
-    <div class="card shadow p-3 mb-5 bg-white rounded">
+    <div class="card shadow  mb-5 bg-white rounded">
                 <div class="card-image">
                 <img class="flag" src="${flag}" alt="Flag">
                 </div>
@@ -48,7 +74,7 @@ function createCard(element) {
                   <p class="card-text"><span>Capital:</span> ${capital}</p>
                 </div>
               </div>
-    `;
+    `
 }
 
-createCard();
+createCard()

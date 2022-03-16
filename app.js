8ff73f50d04545accb67f19a819341c7acb90569
Zybartas Ringys <developerzr92@gmail.com>
2022-03-16 23:21:46 +0200

    daily commit

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-'use strict'
+"use strict";
 
-const cardsContainer = document.getElementById('cards-section')
+const cardsContainer = document.getElementById("cards-section");
 
 // Themes
-const dark = document.getElementById('Dark-mode')
-const body = document.getElementById('body')
-const navCol = document.querySelector('.navigation-col')
-const whereText = document.querySelector('.where')
-const darkText = document.querySelector('.dark')
-const cardBody = document.querySelectorAll('.card-body')
-console.log(cardBody)
+const dark = document.getElementById("Dark-mode");
+const body = document.getElementById("body");
+const navCol = document.querySelector(".navigation-col");
+const whereText = document.querySelector(".where");
+const darkText = document.querySelector(".dark");
+const searchInput = document.querySelector(".search-input");
+const filterBtn = document.getElementById("dropdownMenuButton1");
+const inputGroup = document.querySelector(".input-group");
+const glass = document.querySelector(".fa-magnifying-glass");
+const moon = document.querySelector(".fa-moon");
 
-const url = 'https://restcountries.com/v2/all'
+const url = "https://restcountries.com/v2/all";
 
-let data = {}
+let data = {};
 
 // Dark and light theme
 
-dark.addEventListener('click', switchDark)
+dark.addEventListener("click", switchDark);
 
 // Switch to dark theme function
 
 function switchDark() {
-  body.classList.toggle('active')
-  navCol.classList.toggle('active')
-  whereText.classList.toggle('active')
-  darkText.classList.toggle('active')
+  body.classList.toggle("active");
+  navCol.classList.toggle("active");
+  whereText.classList.toggle("active");
+  darkText.classList.toggle("active");
+  searchInput.classList.toggle("active");
+  filterBtn.classList.toggle("active");
+  inputGroup.classList.toggle("active");
+  glass.classList.toggle("active");
 
-  cardBody.forEach(element => {
-    console.log(element)
-    element.classList.toggle('active')
-  })
+  switchThemeIcon();
+}
+
+// switch theme icon function
+
+function switchThemeIcon() {
+  moon.style.color = "white";
 }
 
 // API for cards functionality
@@ -39,29 +49,42 @@ function switchDark() {
 
 async function fetchCountry() {
   try {
-    const response = await fetch(url)
-    const country = await response.json()
+    const response = await fetch(url);
+    const country = await response.json();
 
-    country.forEach(element => {
-      cardsContainer.insertAdjacentHTML('beforeend', createCard(element))
-    })
+    country.forEach((element) => {
+      cardsContainer.insertAdjacentHTML("beforeend", createCard(element));
+    });
 
-    country.forEach(element => {
-      data[element.name] = element
-    })
+    // cards
+    const cards = document.querySelectorAll(".card-body");
+    const title = document.querySelectorAll(".card-title");
+
+    // cards title
+    title.forEach((element) => {
+      dark.addEventListener("click", function () {
+        element.classList.toggle("active");
+      });
+    });
+    // cards body
+    cards.forEach((element) => {
+      dark.addEventListener("click", function () {
+        element.classList.toggle("active");
+      });
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-fetchCountry()
+fetchCountry();
 
 function createCard(element) {
-  let title = element['name']
-  let flag = element['flag']
-  let population = element['population']
-  let region = element['region']
-  let capital = element['capital']
+  let title = element["name"];
+  let flag = element["flag"];
+  let population = element["population"];
+  let region = element["region"];
+  let capital = element["capital"];
 
   return `
     <div class="card shadow  mb-5 bg-white rounded">
@@ -75,7 +98,5 @@ function createCard(element) {
                   <p class="card-text"><span>Capital:</span> ${capital}</p>
                 </div>
               </div>
-    `
+    `;
 }
-
-createCard()

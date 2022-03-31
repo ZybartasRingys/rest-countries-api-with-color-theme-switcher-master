d933e4ba03d49f489e2175d9c2b2c8e99d471720
Zybartas Ringys <developerzr92@gmail.com>
2022-03-31 21:22:56 +0300

    detail pages

--- js/germany.js
+++ js/germany.js
@@ -36,60 +36,60 @@ async function fetchCountry() {
       });
 
       return `
+      <div class="row">
+      <div class="col-12 mb-5 main-col d-flex flex-column flex-xxl-row justify-content-between">
+        <div class="col col-xxl-5 flag-container">
+          <img class="card-img-top" src="${flag}" alt="Flag" />
+        </div>
 
-        <div class="card d-flex flex-column flex-xxl-row mt-3">
-                  <img
-                    src="${flag}"
-                    class="card-img-top"
-                    alt="..."
-                  />
+        <div class="col col-xxl-6 detail-text d-flex flex-column ">
+          <div
+            class="col text d-flex flex-column flex-xxl-row "
+          >
+            <div class="col-12 first-col">
+              <h5 class="card-title mt-5">${name}</h5>
+              <p class="native mt-4">
+                <span class="bold">Native Name:</span> ${nativeName}
+              </p>
+              <p class="population">
+                <span class="bold">Population:</span> ${population}
+              </p>
+              <p class="region">
+                <span class="bold">Region:</span> ${region}
+              </p>
+              <p class="sub-region">
+                <span class="bold">Sub Region:</span> ${subregion}
+              </p>
+              <p class="capital">
+                <span class="bold">Capital:</span> ${capital}
+              </p>
+            </div>
+            <div class="col second-col">
+              <p class="domain ">
+                <span class="bold">Top Level Domain:</span>
+                ${topLevelDomain}
+              </p>
+              <p class="currencies">
+                <span class="bold">Currencies:</span> ${code}
+              </p>
+              <p class="languages">
+                <span class="bold">Languages:</span>
+                ${languages.name}
+              </p>
+            </div>
+          </div>
 
-                  <div class="detail-cards">
-                  <div class="card-body border mt-4">
-                    <h5 class="card-title">${name}</h5>
-                    <p class="native mt-4">
-                      <span class="bold">Native Name:</span> ${nativeName}
-                    </p>
-                    <p class="population">
-                      <span class="bold">Population:</span> ${population}
-                    </p>
-                    <p class="region">
-                      <span class="bold">Region:</span> ${region}
-                    </p>
-                    <p class="sub-region">
-                      <span class="bold">Sub Region:</span> ${subregion}
-                    </p>
-                    <p class="capital">
-                      <span class="bold">Capital:</span> ${capital}
-                    </p>
-                  </div>
-                  <div class="card-bottom">
-                    <p class="domain">
-                      <span class="bold">Top Level Domain:</span>  ${topLevelDomain}
-                    </p>
-                    <p class="currencies">
-                      <span class="bold">Currencies:</span> ${code}
-                    </p>
-                    <p class="languages">
-                      <span class="bold">Languages:</span> ${languages.name}
-                    </p>
-                  </div>
+          <div class="col borders mt-3 d-flex flex-column flex-xxl-row">
+            <div>
+              <h5 class="border-heading mb-3 ">Border Countries:</h5>
+            </div>
 
-                  <div
-                    class="col-11 borders mb-5 "
-                  >
-                  <div><h5 class="border-heading mb-3">Border Countries: </h5>
-
-                  <div class=" borders-container"></h5>
-                  
-                    </div>
-                  </div>
-                </div>
-                   </div>
-                  
-
-
-                </div>
+            <div class="borders-container"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+        
         `;
     }
 
--- js/united.js
+++ js/united.js
@@ -37,52 +37,59 @@ async function fetchCountry() {
 
       return `
 
-        <div class="card mt-3">
-                  <img
-                    src="${flag}"
-                    class="card-img-top"
-                    alt="..."
-                  />
-                  <div class="card-body mt-4">
-                    <h5 class="card-title">${name}</h5>
-                    <p class="native mt-4">
-                      <span class="bold">Native Name:</span> ${nativeName}
-                    </p>
-                    <p class="population">
-                      <span class="bold">Population:</span> ${population}
-                    </p>
-                    <p class="region">
-                      <span class="bold">Region:</span> ${region}
-                    </p>
-                    <p class="sub-region">
-                      <span class="bold">Sub Region:</span> ${subregion}
-                    </p>
-                    <p class="capital">
-                      <span class="bold">Capital:</span> ${capital}
-                    </p>
-                  </div>
-                  <div class="card-bottom">
-                    <p class="domain">
-                      <span class="bold">Top Level Domain:</span>  ${topLevelDomain}
-                    </p>
-                    <p class="currencies">
-                      <span class="bold">Currencies:</span> ${code}
-                    </p>
-                    <p class="languages">
-                      <span class="bold">Languages:</span> ${languages.name}
-                    </p>
-                  </div>
+      <div class="row">
+      <div class="col-12 mb-5 main-col d-flex flex-column flex-xxl-row justify-content-between">
+        <div class="col col-xxl-5 flag-container">
+          <img class="card-img-top" src="${flag}" alt="Flag" />
+        </div>
 
-                  <div
-                    class="col-11 borders mb-5 "
-                  >
-                  <div><h5 class="border-heading mb-3">Border Countries: </h5>
+        <div class="col col-xxl-6 detail-text d-flex flex-column ">
+          <div
+            class="col text d-flex flex-column flex-xxl-row "
+          >
+            <div class="col-12 first-col">
+              <h5 class="card-title mt-5">${name}</h5>
+              <p class="native mt-4">
+                <span class="bold">Native Name:</span> ${nativeName}
+              </p>
+              <p class="population">
+                <span class="bold">Population:</span> ${population}
+              </p>
+              <p class="region">
+                <span class="bold">Region:</span> ${region}
+              </p>
+              <p class="sub-region">
+                <span class="bold">Sub Region:</span> ${subregion}
+              </p>
+              <p class="capital">
+                <span class="bold">Capital:</span> ${capital}
+              </p>
+            </div>
+            <div class="col second-col">
+              <p class="domain ">
+                <span class="bold">Top Level Domain:</span>
+                ${topLevelDomain}
+              </p>
+              <p class="currencies">
+                <span class="bold">Currencies:</span> ${code}
+              </p>
+              <p class="languages">
+                <span class="bold">Languages:</span>
+                ${languages.name}
+              </p>
+            </div>
+          </div>
 
-                  <div class=" borders-container"></h5>
-                  
-                    
-                  </div>
-                </div>
+          <div class="col borders mt-3 d-flex flex-column flex-xxl-row">
+            <div>
+              <h5 class="border-heading mb-3 ">Border Countries:</h5>
+            </div>
+
+            <div class="borders-container"></div>
+          </div>
+        </div>
+      </div>
+    </div>
         `;
     }
 
